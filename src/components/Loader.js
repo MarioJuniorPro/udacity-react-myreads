b5bc4400b8732560974a3b42b59a6a2f482385d6
Mario Costa Junior <mario.junior.pro@gmail.com>
2018-02-09 01:56:45 -0200

    refactor: Loader function was defined

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,7 @@ import loader from '../icons/loader.svg'
 
 import React from 'react'
 
-export default () => {
+export default function Loader(){
   return (
     <div className="loader">
       <img src={loader} alt="loading" className="loader-icon"/>
