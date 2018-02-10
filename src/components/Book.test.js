ecb161d6008fc168a7bbc806a947e6aaf2e5c4c5
Mario Costa Junior <mario.junior.pro@gmail.com>
2018-02-10 03:32:42 -0200

    test: add test of moving books between shelfs

--- src/components/Book.test.js
+++ src/components/Book.test.js
@@ -5,24 +5,40 @@ import { shallow, mount, render } from 'enzyme'
 
 describe('<Book />', () => {
   const book = {
+    id: 'nggnmAEACAAJ',
     title: 'Learn React',
-    subtitle: 'The essencial guide'
+    subtitle: 'The essencial guide',
+    authors: ['Tolken', 'Dan']
   }
 
+  const mockBookMove = jest.fn()
+
   it('renders without crashing', () => {
-    const wrapper = shallow(<Book book={book}/>)
+    const wrapper = shallow(<Book book={book} onMove={mockBookMove} />)
     expect(wrapper).toHaveLength(1)
   })
 
   it('render a title', () => {
-    const wrapper = mount(<Book book={book}/>)
+    const wrapper = mount(<Book book={book} onMove={mockBookMove} />)
     expect(wrapper.find('.book-title').text()).toBe(book.title)
   })
 
-  it('render a cover', () => {
-    const wrapper = mount(<Book book={book}/>)
+  it('render a <BookCover />', () => {
+    const wrapper = mount(<Book book={book} onMove={mockBookMove} />)
     const cover = wrapper.find('BookCover')
     expect(cover).toHaveLength(1)
+  })
+
+  it('render a <BookAuthors />', () => {
+    const wrapper = mount(<Book book={book} onMove={mockBookMove} />)
+    const comp = wrapper.find('BookAuthors')
+    expect(comp).toHaveLength(1)
+  })
+
+  it('render a <BookShelfChanger />', () => {
+    const wrapper = mount(<Book book={book} onMove={mockBookMove}/>)
+    const comp = wrapper.find('BookShelfChanger')
+    expect(comp).toHaveLength(1)
     // console.log(wrapper.debug())
   })
 })
