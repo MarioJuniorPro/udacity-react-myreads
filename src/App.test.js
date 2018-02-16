e15e45f39169dd2c1750c88c1540adab1fd5a144
Mario Costa Junior <mario.junior.pro@gmail.com>
2018-02-15 22:38:56 -0200

    feature: use url query param to fetch search

--- src/App.test.js
+++ src/App.test.js
@@ -26,38 +26,11 @@ describe('<App />', () => {
   })
 
   it('show loader', () => {
-    expect.assertions(2);
+    expect.assertions(2)
 
     const wrapper = mount(<App api={mockAPI} />)
-    // console.log(wrapper.debug())
-    // console.log(
-    //   wrapper.find('Router'),
-    //   wrapper.find('Loader'),
-    //   wrapper.find('.app')
-    // )
-    // console.log(wrapper.instance().state)
 
     expect(wrapper.find('Loader')).toHaveLength(1)
     expect(wrapper.find('ListBooks')).toHaveLength(0)
   })
-
-  // it('should crash without api', () => {
-  //   // const wrapper = mount(<App />)
-
-  //   const renderComponent = () => {
-  //     try {
-  //       const wrapper = mount(<App />)
-  //       wrapper.instance()
-  //     } catch (error) {
-  //       console.log(wrapper)
-  //       console.error(error)
-  //       throw new Error(error)
-  //     }
-  //   }
-  //   expect(renderComponent).toThrow()
-
-  // })
-  // console.log(wrapperShallow.debug())
-  // console.log(wrapperMount.debug())
-  // console.log(wrapperRender.html())
 })
--- src/components/SearchBooks.test.js
+++ src/components/SearchBooks.test.js
@@ -4,10 +4,7 @@ import { MemoryRouter } from 'react-router-dom'
 
 import SearchBooks from './SearchBooks'
 
-jest.useFakeTimers()
-
 describe('<SearchBooks />', () => {
-
   const books = [
     {
       id: 'nggnmAEACAAJ',
@@ -27,27 +24,42 @@ describe('<SearchBooks />', () => {
   const book = books[0]
 
   it('renders without crashing', () => {
-    const wrapper = shallow(<SearchBooks />)
+    const mockSearchBooks = jest.fn()
+    const wrapper = shallow(<SearchBooks searchBooks={mockSearchBooks} />)
     expect(wrapper).toHaveLength(1)
   })
 
   it('render a grid of books', () => {
+    const mockSearchBooks = jest.fn()
     const wrapper = mount(
       <MemoryRouter>
-        <SearchBooks />
+        <SearchBooks searchBooks={mockSearchBooks} />
       </MemoryRouter>
     )
     expect(wrapper.find('BookGrid')).toHaveLength(1)
   })
 
-  it('search by typing 2 or more characteres', async () => {
-    const mockSearchBooks = jest.fn().mockReturnValue(Promise.resolve(books))
-    const wrapper = shallow(<SearchBooks searchBooks={mockSearchBooks} />)
-    wrapper
+  it('search by typing 2 or more characteres', async (done) => {
+    expect.assertions(2)
+    const mockSearchBooks = jest.fn().mockImplementation((...arg) => {
+    })
+    const wrapper = mount(
+      <MemoryRouter>
+        <SearchBooks books={[]} searchBooks={mockSearchBooks} />
+      </MemoryRouter>
+    )
+
+    expect(mockSearchBooks.mock.calls).toHaveLength(1)
+    const searchBooksWrapper = wrapper.find('SearchBooks')
+    searchBooksWrapper
       .find('DebounceInput')
       .simulate('change', { target: { value: 'React' } })
-    expect(mockSearchBooks.mock.calls).toHaveLength(1)
-  })
+
+    await setTimeout(() => {
+      expect(mockSearchBooks.mock.calls).toHaveLength(2)
+      done()
+    }, 1000)
+  }, 5000)
 
   it('search for books', async () => {
     const mockSearchBooks = jest.fn().mockReturnValue(Promise.resolve(books))
@@ -56,19 +68,27 @@ describe('<SearchBooks />', () => {
         <SearchBooks books={[]} searchBooks={mockSearchBooks} />
       </MemoryRouter>
     )
-    const booksBeforeSearch = wrapper.find('BookGrid').find('Book')
 
     const searchBooksWrapper = wrapper.find('SearchBooks')
+    const booksBeforeSearch = wrapper.find('BookGrid').find('Book')
     expect(booksBeforeSearch).toHaveLength(0)
 
-    searchBooksWrapper.instance().setSearchTerm('')
     expect(mockSearchBooks.mock.calls).toHaveLength(1)
+    searchBooksWrapper.instance().setSearchTerm('')
+    expect(mockSearchBooks.mock.calls).toHaveLength(2)
   })
 
   it('move books to shelfs', async () => {
     const mockUpdateBook = jest.fn()
-    const wrapper = shallow(<SearchBooks books={books} updateBook={mockUpdateBook} />)
-    wrapper.instance().moveBook(book, 'wantToRead')
+    const mockSearchBooks = jest.fn()
+    const wrapper = mount(
+      <MemoryRouter>
+        <SearchBooks books={books} updateBook={mockUpdateBook} searchBooks={mockSearchBooks}/>
+      </MemoryRouter>
+    )
+
+    const searchBooksWrapper = wrapper.find('SearchBooks')
+    searchBooksWrapper.instance().moveBook(book, 'wantToRead')
     expect(mockUpdateBook.mock.calls).toHaveLength(1)
     expect(mockUpdateBook.mock.calls[0][0]).toMatchObject({
       ...book,
